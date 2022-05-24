b0b72477524f4a65877fa22630ed0c884a91e523
dovydaszemait <dovydas.zemas@gmail.com>
2022-05-24 18:01:52 +0300

    main pages/login/register/home

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@mui/icons-material";
 import React from "react";
 import Announcement from "../../components/announcement/Announcement";
 import Footer from "../../components/footer/Footer";
@@ -31,16 +32,68 @@ export default function Cart() {
                   <S.ProductId>
                     <b>ID:</b>65765858595
                   </S.ProductId>
-                  <S.ProductColor />
+                  <S.ProductColor color="black" />
                   <S.ProductSize>
                     <b>Weight:</b>600g
                   </S.ProductSize>
                 </S.Details>
               </S.ProductDetail>
-              <S.PriceDetail>price</S.PriceDetail>
+              <S.PriceDetail>
+                <S.ProductAmountContainer>
+                  <Add />
+                  <S.ProductAmount>2</S.ProductAmount>
+                  <Remove />
+                </S.ProductAmountContainer>
+                <S.ProductPrice>$ 30</S.ProductPrice>
+              </S.PriceDetail>
+            </S.Product>
+            <S.Hr />
+            <S.Product>
+              <S.ProductDetail>
+                <S.Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
+                <S.Details>
+                  <S.ProductName>
+                    <b>Product:</b>BURGEER
+                  </S.ProductName>
+                  <S.ProductId>
+                    <b>ID:</b>2332323445
+                  </S.ProductId>
+                  <S.ProductColor color="red" />
+                  <S.ProductSize>
+                    <b>Weight:</b>12600g
+                  </S.ProductSize>
+                </S.Details>
+              </S.ProductDetail>
+              <S.PriceDetail>
+                <S.ProductAmountContainer>
+                  <Add />
+                  <S.ProductAmount>2</S.ProductAmount>
+                  <Remove />
+                </S.ProductAmountContainer>
+                <S.ProductPrice>$ 30</S.ProductPrice>
+              </S.PriceDetail>
             </S.Product>
           </S.Info>
-          <S.Summary></S.Summary>
+          <S.Summary>
+            <S.SummaryTitle>ORDER SUMMARY</S.SummaryTitle>
+            <S.SummaryItem>
+              <S.SummaryItemText>Subtotal</S.SummaryItemText>
+              <S.SummaryItemText>$ 80</S.SummaryItemText>
+            </S.SummaryItem>
+            <S.SummaryItem>
+              <S.SummaryItemText>Estimated shipping</S.SummaryItemText>
+              <S.SummaryItemText>$ 5.90</S.SummaryItemText>
+            </S.SummaryItem>
+            <S.SummaryItem>
+              <S.SummaryItemText>Shipping Discount</S.SummaryItemText>
+              <S.SummaryItemText>$ 5.90 </S.SummaryItemText>
+            </S.SummaryItem>
+            <S.SummaryItem>
+              <S.SummaryItemText type="total">Total</S.SummaryItemText>
+              <S.SummaryItemText>$ 80</S.SummaryItemText>
+            </S.SummaryItem>
+            <S.Button>Checkuot</S.Button>
+          </S.Summary>
         </S.Bottom>
       </S.Wrapper>
       <Footer />
